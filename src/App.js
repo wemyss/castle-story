b3e58b66eb69856e19acf6059d9a9bc2fd89cab3
Sam Wemyss <samuel.wemyss@gmail.com>
2017-09-20 23:53:00 +1000

    add description of what the tool does and clean some stuff up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './registerServiceWorker';
 import Checkbox from './Checkbox';
 import './App.css';
 import logo from './logo.png';
+import github from './github.svg';
 
 const gameObjects = [
   {
@@ -43,12 +44,22 @@ class App extends Component {
 
     this.state = { uploadedGameObjects: '', extractedObjects: '' }
 
+    this.selectAll = this.selectAll.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.updateCheckbox = this.updateCheckbox.bind(this)
     this.updateUserGameObjects = this.updateUserGameObjects.bind(this)
   }
 
-  handleSubmit = (event) => {
+  selectAll() {
+    this.setState({
+      ...gameObjects.reduce((acc, elem) => {
+        acc[elem.name] = true
+        return acc
+      }, {})
+    })
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
     let data = []
     try {
@@ -89,18 +100,26 @@ class App extends Component {
     this.setState({ uploadedGameObjects: event.target.value })
   }
 
+  componentWillMount() {
+    this.selectAll()
+  }
+
   render() {
     return (
       <div>
-        <div className='App'>
-          <div className='App-header'>
-            <img src={logo} className='App-logo' alt='Castle Story' />
+        <div className='App-header'>
+          <div class='container'>
+            <a href="//www.castlestory.net">
+              <img src={logo} className='App-logo' alt='Castle Story' />
+            </a>
             <h1>Game Object Extractor</h1>
+            <p>A simple tool to help you pull out only the objects you want from <code>gameobjects.json</code> so that you can transfer them into a maps <code>gameobjects.json</code> without it breaking or having all these extra objects (bricktron spirits, crystal spawns, etc) you don't want.</p>
           </div>
+        </div>
+
+        <div class='container'>
           <div className='App-intro'>
-            <p>
-              To get started, paste the contents of your <code>gameobjects.json</code> below.
-            </p>
+            <p>To get started, select the objects you want to keep and paste the contents of your <code>gameobjects.json</code> below.</p>
             <form onSubmit={this.handleSubmit}>
               {gameObjects.map(g =>
                   <Checkbox key={g.name} label={g.displayName} checked={this.state[g.name]} onChange={() => this.updateCheckbox(g.name)} />
@@ -112,9 +131,10 @@ class App extends Component {
               <br />
               <button type='submit'>EXTRACT OBJECTS</button>
             </form>
-            <p>Now copy the outputted JSON below and paste at the end of the <code>gameobjects.json</code> for your map, just before the last character - which would be a <code>]</code> (make sure you put a comma before the last <code>}</code> as well - you can validate your JSON online)</p>
+            <p>Now just copy the outputted JSON below and paste it at the end of the <code>gameobjects.json</code> for your map, just before the last character - which would be a <code>]</code> (make sure you put a comma before the last <code>}</code> as well - you can validate your JSON online)</p>
             <textarea value={this.state.extractedObjects} readonly></textarea>
           </div>
+
           <hr/>
           <h3>Notes</h3>
           <ul>
@@ -122,7 +142,14 @@ class App extends Component {
             <li>The outputted JSON is invalid because I've unwrapped it from it's array, make sure to insert it inside of the array in <code>gameobjects.json</code></li>
           </ul>
         </div>
+
         <div className='footer'>
+          <div>
+            <a href="//github.com/wemyss/castle-story">
+              <img src={github} alt="Github repo" class="social-icon" />
+            </a>
+          </div>
+          <br/>
           <small>Developed by <a href="https://wemyss.github.io">Sam Wemyss</a></small>
         </div>
       </div>
